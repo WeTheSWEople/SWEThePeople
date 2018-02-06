b603942252e68da48f3e7f7b44bdd9cdef3eaa3c
copperstick6 <copperstick6@gmail.com>
2018-02-05 21:22:05 -0600

    added gitignore and dsstore

---
+++ router.py
@@ -0,0 +1,20 @@
+import os
+from flask import Flask, send_from_directory
+
+app = Flask(__name__, static_folder='idb/build')
+
+# Serve React App
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def serve(path):
+    if(path == ""):
+        return send_from_directory('idb/build', 'index.html')
+    else:
+        if(os.path.exists("idb/build/" + path)):
+            return send_from_directory('idb/build', path)
+        else:
+            return send_from_directory('idb/build', 'index.html')
+
+
+if __name__ == '__main__':
+    app.run(use_reloader=True, port=5000, threaded=True)
