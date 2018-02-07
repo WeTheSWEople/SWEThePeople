0ee2327b6968dd560c81e48c054b330d614e77df
palakhirpara <palak_hirpara@yahoo.com>
2018-02-07 16:09:21 -0600

    Added carousel functionality with 3 images

--- idb/src/Splash.js
+++ idb/src/Splash.js
@@ -1,20 +1,57 @@
 import React, { Component } from 'react';
+import Slider from 'react-slick';
 import logo from './logo.svg';
+import pic1 from './assets/pic1.jpg'
+import pic2 from './assets/pic2.jpg'
+import pic3 from './assets/pic3.jpg'
 import './App.css';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 
 export default class Splash extends Component {
+  constructor(props) {
+    super(props);
+    
+    this.settings = {
+      dots: true,
+      infinite: true,
+      speed: 500,
+      arrows: true,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      autoplay: true,
+      centerMode: true,
+      autoplaySpeed: 4000,
+      centerPadding: '420px'
+    };
+  }
   render() {
+    
     return (
       <div className="App">
-        <header className="App-header">
+      <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Splash Screen One</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <Slider {...this.settings}>
+        <div >
+            <img src={pic1}  alt="" />
+                    
+        </div>
+       <div >
+            <img src={pic2}  alt="" />
+                    
+        </div>
+        <div >
+            <img src={pic3}  alt="" />
+                    
+        </div>
+    
+      </Slider>
       </div>
     );
   }
 }
+
+
