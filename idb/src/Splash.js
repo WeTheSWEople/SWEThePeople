35a0967ef865af814aeafd50af662a6f0dcc4ecf
bzsinger <benjamin.z.singer@gmail.com>
2018-02-07 16:32:05 -0600

    Replace header image

--- idb/src/Splash.js
+++ idb/src/Splash.js
@@ -4,6 +4,7 @@ import logo from './logo.svg';
 import pic1 from './assets/pic1.jpg'
 import pic2 from './assets/pic2.jpg'
 import pic3 from './assets/pic3.jpg'
+import header from './assets/header.png'
 import './App.css';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
@@ -12,46 +13,44 @@ import "slick-carousel/slick/slick-theme.css";
 export default class Splash extends Component {
   constructor(props) {
     super(props);
-    
+
     this.settings = {
       dots: true,
       infinite: true,
-      speed: 500,
+      speed: 300,
       arrows: true,
       slidesToShow: 1,
       slidesToScroll: 1,
       autoplay: true,
       centerMode: true,
       autoplaySpeed: 4000,
-      centerPadding: '420px'
+      centerPadding: '420px',
+      pauseOnHover: true
     };
   }
   render() {
-    
+
     return (
       <div className="App">
       <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Splash Screen One</h1>
+          <img src={header} className="App-logo" alt="logo" />
         </header>
       <Slider {...this.settings}>
         <div >
             <img src={pic1}  alt="" />
-                    
+
         </div>
        <div >
             <img src={pic2}  alt="" />
-                    
+
         </div>
         <div >
             <img src={pic3}  alt="" />
-                    
+
         </div>
-    
+
       </Slider>
       </div>
     );
   }
 }
-
-
