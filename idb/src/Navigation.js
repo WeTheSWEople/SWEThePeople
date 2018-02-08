2d627103ed60a034b5864a85daee7230ca5d7ccf
raulcodes <rcamacho96@gmail.com>
2018-02-07 18:50:29 -0600

    added navbar component

---
+++ idb/src/Navigation.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import { Navbar, Nav, NavItem } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap'
+
+export default class Navigation extends Component {
+  render() {
+    return (
+      <div>
+        <Navbar>
+          <Navbar.Header>
+            <Navbar.Brand>
+              <LinkContainer to="/">
+                <a>Swe The People</a>
+              </LinkContainer>
+            </Navbar.Brand>
+          </Navbar.Header>
+          <Nav>
+            <LinkContainer to="/">
+              <NavItem>
+                Home
+              </NavItem>
+            </LinkContainer>
+            <LinkContainer to="/about">
+              <NavItem>
+                About
+              </NavItem>
+            </LinkContainer>
+          </Nav>
+        </Navbar>
+      </div>
+    );
+  }
+}
+
+
