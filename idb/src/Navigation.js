99073a35d256b103bdab5b3849f545a5a9b5eeea
Michael Tirtowidjojo <mtirtowidjojo@gmail.com>
2018-02-11 11:19:59 -0600

    Add parties index to the nav bar

--- idb/src/Navigation.js
+++ idb/src/Navigation.js
@@ -33,6 +33,11 @@ export default class Navigation extends Component {
                 Representatives
               </NavItem>
             </LinkContainer>
+            <LinkContainer to="/parties">
+              <NavItem>
+                Parties
+              </NavItem>
+            </LinkContainer>
           </Nav>
         </Navbar>
       </div>
