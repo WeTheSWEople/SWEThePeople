3ae479f7205dda55c9c8dbb54c68a0508a2dc410
palakhirpara <palak_hirpara@yahoo.com>
2018-02-11 17:58:46 -0600

    Added bootstrap rows and cols to the rep details page

--- idb/src/RepresentativeDetails.js
+++ idb/src/RepresentativeDetails.js
@@ -4,6 +4,8 @@ import './App.css';
 import allReps from './assets/bioguide-endpoint.json';
 import { Link } from 'react-router-dom'
 import {Timeline} from 'react-twitter-widgets'
+import { Grid, Row, Col } from 'react-bootstrap';
+
 
 const styles = {
   hyperlink: {
@@ -34,33 +36,45 @@ export default class RepresentativeDetails extends Component {
     // console.log("All reps: " + allReps[this.state.bioguideid])
     console.log("rep data: " + this.state.rep_data["last-name"])
     return (
+
       <div className="App">
       <header className="Rep-Details-header-white"> </header>
-      <img src={"https://theunitedstates.io/images/congress/225x275/" + this.state.bioguideid + ".jpg"}  alt="" />
-      <p style={{paddingTop:"10px"}}>{this.state.rep_data["first-name"]} {this.state.rep_data["last-name"]}   </p>
-      <p> Party: <Link
-        to={`/parties/${this.state.rep_data["party"]}`}>
-        {this.state.rep_data["party"]} </Link> 
-      </p>
-      <p> State: {this.state.rep_data["state"]}</p>
-      <p> District: <Link
-        to={`/districts/${this.state.rep_data["state"]}/${this.state.rep_data["district"]}`}>
-        {this.state.rep_data["district"]} </Link> 
-      </p>
-      <a href={this.state.rep_data["url"]}>Website</a>
-      <br></br>
-      <Timeline
-       dataSource={{
-         sourceType: "profile",
-         screenName: this.state.rep_data["twitter"]
-       }}
-       options={{
-         username: this.state.rep_data["twitter"],
-         height: "400",
-         width:"400"
+      <Row>
+        <Col sm={12} md={4}>
+          <img src={"https://theunitedstates.io/images/congress/225x275/" + this.state.bioguideid + ".jpg"}  alt="" />
+        </Col>
+        <Col sm={12} md={4}>
+          <div style={{textAlign: "left"}}>
+          <p style={{paddingTop:"10px"}}>{this.state.rep_data["first-name"]} {this.state.rep_data["last-name"]}   </p>
+          <p> Party: <Link
+            to={`/parties/${this.state.rep_data["party"]}`}>
+            {this.state.rep_data["party"]} </Link> 
+          </p>
+          <p> State: {this.state.rep_data["state"]}</p>
+          <p> District: <Link
+            to={`/districts/${this.state.rep_data["state"]}/${this.state.rep_data["district"]}`}>
+            {this.state.rep_data["district"]} </Link> 
+          </p>
+          <a href={this.state.rep_data["url"]}>Website</a>
+          </div>
+        </Col>
 
-       }}
-     />
+        <Col sm={12} md={4}>
+          <Timeline
+           dataSource={{
+             sourceType: "profile",
+             screenName: this.state.rep_data["twitter"]
+           }}
+           options={{
+             username: this.state.rep_data["twitter"],
+             height: "400",
+             width:"400"
+           }}
+         />
+        </Col>
+
+      </Row>
+      
       </div>
     );
   }
