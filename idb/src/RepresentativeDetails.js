93177317c20370c5ca25d5f1bbe2312e82a5912a
bzsinger <benjamin.z.singer@gmail.com>
2018-02-11 17:36:10 -0600

    Removed cruft

--- idb/src/RepresentativeDetails.js
+++ idb/src/RepresentativeDetails.js
@@ -26,13 +26,10 @@ export default class RepresentativeDetails extends Component {
     // get the data - in the future call the api
     this.setState({bioguideid: this.props.match.params.bioguideid})
     this.setState({rep_data: allReps[this.props.match.params.bioguideid]})
-    
+
   }
-  
+
   render() {
-    console.log("bioguideid: " + this.state.bioguideid)
-    // console.log("All reps: " + allReps[this.state.bioguideid])
-    console.log("rep data: " + this.state.rep_data["last-name"])
     return (
       <div className="App">
       <header className="Rep-Details-header-white"> </header>
@@ -40,12 +37,12 @@ export default class RepresentativeDetails extends Component {
       <p style={{paddingTop:"10px"}}>{this.state.rep_data["first-name"]} {this.state.rep_data["last-name"]}   </p>
       <p> Party: <Link
         to={`/parties/${this.state.rep_data["party"]}`}>
-        {this.state.rep_data["party"]} </Link> 
+        {this.state.rep_data["party"]} </Link>
       </p>
       <p> State: {this.state.rep_data["state"]}</p>
       <p> District: <Link
         to={`/districts/${this.state.rep_data["state"]}/${this.state.rep_data["district"]}`}>
-        {this.state.rep_data["district"]} </Link> 
+        {this.state.rep_data["district"]} </Link>
       </p>
       <a href={this.state.rep_data["url"]}>Website</a>
       <br></br>
