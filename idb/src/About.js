b2e41262cbfdb2028968e59ceb7414e3b15de0c5
Michael Tirtowidjojo <mtirtowidjojo@gmail.com>
2018-02-14 17:52:18 -0600

    Beautify the About page
    references #75

--- idb/src/About.js
+++ idb/src/About.js
@@ -29,12 +29,12 @@ export default class About extends Component {
 				this.setState({error: true, ready: true})
 			}
 			var swe_members = {}
-			swe_members['MTirtowidjojo'] = ['Michael Tirtowidjojo', 0,0, 0,"Michael is a third-year CS student who trains in Taekwondo and enjoys reading World War II stories.", "Michael.png"]
-			swe_members['copperstick6'] = ["William Han",0,0, 0, "William is a sophomore CS student who enjoys the subtle art of memes and hackathons.", "William.jpg"]
-			swe_members['raulcodes'] = ['Raul Camacho', 0,0, 0, "Raul is a senior CS major who will be graduating this semester. Also he's tall. Probably too tall.", "Raul.png"]
-			swe_members['minwoo0jo'] = ['Minwoo Jo', 0,0, 0, "Minwoo is a fourth year student currently pursuing a BSA in CS. He enjoys studying foreign languages and competing in video game tournaments in his free time.", 'Minwoo.jpg']
-			swe_members['bzsinger'] = ['Benjamin Singer', 0,0, 0, "Benny is a third-year CS student who enjoys iOS development, reading, and following current events.", "Benny.jpg"]
-			swe_members['palakhirpara'] = ['Palakkumar Hirpara', 0, 0, 0, "Palak is a senior who will be graduating this semester with BSCS and likes watching cricket.", "Palak.png"]
+			swe_members['MTirtowidjojo'] = ['Michael Tirtowidjojo', 0,0, 0,"Michael is a third-year CS student who trains in Taekwondo and enjoys reading World War II stories.", "Michael.png", "Parties front-end and chief of chief naming", "Mojo Jojo"]
+			swe_members['copperstick6'] = ["William Han",0,0, 0, "William is a sophomore CS student who enjoys the subtle art of memes and hackathons.", "William.jpg", "Districts front-end, production deployment, and chief of nocturnal development", "Bill"]
+			swe_members['raulcodes'] = ['Raul Camacho', 0,0, 0, "Raul is a senior CS major who will be graduating this semester. Also he's tall. Probably too tall.", "Raul.png", "Front-end beautifier and chief ducker of low ceilings", "Camacho Style Sheets"]
+			swe_members['minwoo0jo'] = ['Minwoo Jo', 0,0, 0, "Minwoo is a fourth year student currently pursuing a BSA in CS. He enjoys studying foreign languages and competing in video game tournaments in his free time.", 'Minwoo.jpg', "District front-end, API designer, and chief late team addition", "MJ"]
+			swe_members['bzsinger'] = ['Benjamin Singer', 0,0, 0, "Benny is a third-year CS student who enjoys iOS development, reading, and following current events.", "Benny.jpg", "Representatives front-end and chief spammer of GitHub emails", "Benny"]
+			swe_members['palakhirpara'] = ['Palak Hirpara', 0, 0, 0, "Palak is a senior who will be graduating this semester with BSCS and likes watching cricket.", "Palak.png", "Representatives front-end and chief misspeller of routes", "PK Attribute"]
 			var commit_json = JSON.parse(body)
 			var total_commits = 0
 			for(var i = 0; i < commit_json.length; i++){
@@ -79,29 +79,76 @@ export default class About extends Component {
 	  let calls_ready = null
 	  let members = null
 	  if(this.state.ready){
-		  calls_ready = <h4>Total Commits: {this.state.total_commits} <br />Total Issues: {this.state.total_issues} <br />Total Unit tests: 0</h4>
+		  calls_ready = <h4>Total Commits: {this.state.total_commits} <br />Total Issues: {this.state.total_issues} <br />Total Unit Tests: 0</h4>
 		  members = <Members swe_data = {this.state.swe_member_data} />
 	  }
 
     return (
-      <div className="App">
-			<header className="About-header-white">
-      </header>
-      <h2>WeTheSWEople Present: </h2>
-      <h3>SWEThePeople.me</h3>
-      <h4>SWEThePeople provides information and resources for anyone interested in the members of United States House of Representatives. This site provides information about representatives, their districts, and parties.</h4>
-      <h4>The site combines information about legislators with U.S. Census data, providing visitors with a better understanding of the intersection between party, state, and socioeconomics.</h4>
-      <center><RingLoader
-        color={'#123abc'}
-        loading={!this.state.ready}
-      /></center>
-      {calls_ready}
-      {members}
-      <p>
-        <a href = "https://github.com/WeTheSWEople/SWEThePeople/">
-            Link to the GitHub Repo
-        </a>
-      </p>
+      <div className="App container about-content">
+            <h1>swethepeople.me</h1>
+            <h3>Brought to you by WeTheSWEople</h3>
+            <div className="row about-info">
+                <div className="col-sm-4 col-sm-offset-2">
+                    <h4>
+                        SWEThePeople provides information and resources for
+                        anyone interested in the members of United States House
+                        of Representatives. This site provides information about
+                        representatives, their districts, and parties.
+                    </h4>
+                </div>
+                <div className="col-sm-4">
+                    <h4>
+                        The site combines information about legislators with
+                        U.S. Census data, providing visitors with a better
+                        understanding of the intersection between party, state,
+                        and demographics.
+                    </h4>
+                </div>
+            </div>
+            <center>
+                <RingLoader color={'#123abc'} loading={!this.state.ready} />
+            </center>
+            <div className="github-info">
+                <div className="row">
+                    <div className="col-sm-4 col-sm-offset-4">
+                        <div className="github-counts">
+                            <h4>
+                                Total Commits:
+                                <span className="right-info">
+                                    {this.state.total_commits}
+                                </span>
+                            </h4>
+                            <h4>
+                                Total Issues:
+                                <span className="right-info">
+                                    {this.state.total_issues}
+                                </span>
+                            </h4>
+                            <h4>
+                                Total Unit Tests:
+                                <span className="right-info">0</span>
+                            </h4>
+                        </div>
+                    </div>
+                </div>
+                <p>
+                    <a href = "https://github.com/WeTheSWEople/SWEThePeople/">
+                        GitHub Repository
+                    </a>
+                </p>
+                <p>
+                    <a href = "https://www.gitbook.com/@wethesweople">
+                        GitBook Documentation
+                    </a>
+                </p>
+            </div>
+        
+            <div className="about-header">
+                <h1>Your Representatives</h1>
+            </div>
+            {members}
+          <p>
+          </p>
 
         <div className="tools-div container">
             <div className="tools-header">
@@ -121,7 +168,7 @@ export default class About extends Component {
                         </div>
                     </div>
                 </a>
-                <a href="">
+                <a href="https://www.gitbook.com/@wethesweople">
                     <div className="col-sm-3">
                         <div className="tools-card">
                             <img src={require("./assets/images/about/gitbook.png")}
--- idb/src/Members.js
+++ idb/src/Members.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Grid, Row, Col } from 'react-bootstrap';
+import './Members.css';
 
 export default class Members extends Component {
 	constructor(props){
@@ -7,17 +8,26 @@ export default class Members extends Component {
 	}
   render() {
 		let mapping = Object.keys(this.props.swe_data).map((item) =>
-	  <Col sm={12} md={4}>
-	  <h3>{this.props.swe_data[item][0]} <br /></h3>
-	  <p><img src={require("./assets/images/about/" + this.props.swe_data[item][5])} width='300' height='300' />
-	  <br />
-	  <br />
-	  {this.props.swe_data[item][4]}<br />
-	  Commits: {this.props.swe_data[item][1]} <br />
-	  Issues: {this.props.swe_data[item][2]} <br />
-	  Unit Tests: {this.props.swe_data[item][3]}</p>
-	  </Col>
-	)
+            <Col sm={12} md={4}>
+                <div className="member-card">
+                    <h3>{this.props.swe_data[item][0]} <br /></h3>
+                    <p className="nickname">
+                        {'"' + this.props.swe_data[item][7] + '"'}
+                    </p>
+                    <img src={require("./assets/images/about/" + this.props.swe_data[item][5])}
+                         width='300' height='300' />
+
+                    <br />
+                    <p>{this.props.swe_data[item][6]}</p>
+                    <p>{this.props.swe_data[item][4]}</p>
+                    <p>
+                        Commits: {this.props.swe_data[item][1]}<br />
+                        Issues: {this.props.swe_data[item][2]}<br />
+                        Unit Tests: {this.props.swe_data[item][3]}<br />
+                    </p>
+                </div>
+            </Col>
+        )
 	return (
 	  <Grid>
 			<Row>
