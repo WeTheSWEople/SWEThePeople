04af46de69aa6a0c03df2c18e7368802248a64bd
copperstick6 <copperstick6@gmail.com>
2018-02-14 02:09:11 -0600

    added some dynamic content to site involving legislature data

--- state_data_scraper.py
+++ state_data_scraper.py
@@ -6,13 +6,14 @@ import apikeys
 data = json.load(open('state.json'))
 count = 1
 for i in data:
-	count+=1
-	url = "https://api.propublica.org/congress/v1/members/senate/" + i + "/current.json"
+	print(i)
+	url = "http://openstates.org/api/v1/metadata/" + i
 
 	headers = {
-		'x-api-key': apikeys.rep_key(),
+		'x-api-key': apikeys.state_key()
 		}
 
 	response = requests.request("GET", url, headers=headers)
-	with open(os.path.join('./idb/src/assets/data/senate_data',str(i) + ".json"), "w") as file1:
+
+	with open(os.path.join('./idb/src/assets/data/state_data',str(i) + ".json"), "w") as file1:
 		file1.write(response.text.encode('utf-8'))
