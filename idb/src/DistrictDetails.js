34ed2ff9f739415853c289bdbc48e54ba09537db
palakhirpara <palak_hirpara@yahoo.com>
2018-02-14 18:07:03 -0600

    adding rep images to district details

--- idb/src/DistrictDetails.js
+++ idb/src/DistrictDetails.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import header from './assets/images/header.png'
 import './App.css';
-import stateDistrict from './assets/state-district-endpoint.json';
+import stateDistrict from './assets/data/census-data.json';
 import { Link } from 'react-router-dom'
 import {Timeline} from 'react-twitter-widgets'
 import { Grid, Row, Col, ProgressBar } from 'react-bootstrap';
 import allReps from './assets/bioguide-endpoint.json';
 import states from './assets/data/state.json'
+import RepresentativeInstance from './RepresentativeInstance'
+import './DistrictDetails.css';
 
 
 const styles = {
@@ -40,6 +42,10 @@ export default class RepresentativeDetails extends Component {
   }
 
   render() {
+    var reps_grid = <div class="col-sm-3 district-rep-card">
+            <RepresentativeInstance rep={this.state.reps[key]} />
+    </div>
+
     console.log(this.state.district_state + " " + this.state.district_num + JSON.stringify(this.state.district_data["representative"]))
     return (
 
@@ -50,7 +56,23 @@ export default class RepresentativeDetails extends Component {
           <h1>{states[this.state.district_state]} District {this.state.district_num}</h1>
           <img src={"https://theunitedstates.io/images/congress/225x275/" + this.state.district_data["bioguide"] + ".jpg"}  height="110px" width="90px" alt="" style={styles.roundcorner} />
           <div>{this.state.district_data["representative"]} </div>
-
+          </Col>
+        </Row>
+        <Row >
+          <Col sm={6} md={6}>
+          <h4><b>Population: </b>{this.state.district_data["population"]["total"]} </h4>
+          <img src={"https://theunitedstates.io/images/congress/225x275/" + this.state.district_data["bioguide"] + ".jpg"}  height="110px" width="90px" alt="" style={styles.roundcorner} />
+          <div>{this.state.district_data["representative"]} </div>
+          </Col>
+          <Col sm={6} md={6}>
+          <h1>{states[this.state.district_state]} District {this.state.district_num}</h1>
+          <img src={"https://theunitedstates.io/images/congress/225x275/" + this.state.district_data["bioguide"] + ".jpg"}  height="110px" width="90px" alt="" style={styles.roundcorner} />
+          <div>{this.state.district_data["representative"]} </div>
+          </Col>
+        </Row>
+        <Row>
+          <Col sm={6} md={6}>
+          {reps_grid}
           </Col>
         </Row>
 
