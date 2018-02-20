9a96fa7ac3a31a3651b8c413509f9530fee0d1e3
bzsinger <benjamin.z.singer@gmail.com>
2018-02-20 17:46:58 -0600

    Added Districts linter changes

--- idb/src/js/Districts/AllDistricts.js
+++ idb/src/js/Districts/AllDistricts.js
@@ -1,39 +1,41 @@
+/* eslint-disable no-unused-vars */
+import React, {Component} from 'react'
 import StateInstance from '../Representatives/StateInstance'
-import '../../assets/css/App.css';
-import React, { Component } from 'react';
-import {GridList} from 'material-ui/GridList';
+import {GridList} from 'material-ui/GridList'
+/* eslint-enable no-unused-vars */
+
+import '../../assets/css/App.css'
 
 const styles = {
   root: {
-	paddingTop: '50px',
-	paddingLeft: '50px',
-	paddingRight: '50px',
-	justifyContent: 'space-around',
+    paddingTop: '50px',
+    paddingLeft: '50px',
+    paddingRight: '50px',
+    justifyContent: 'space-around'
   },
   gridList: {
-	width: '80%',
-	height: '100%',
-	overflowY: 'auto',
-  },
-};
+    width: '80%',
+    height: '100%',
+    overflowY: 'auto'
+  }
+}
 
-let state_json = require('../../assets/data/state.json')
+let stateJSON = require('../../assets/data/state.json')
 export default class AllDistricts extends Component {
-
-  render() {
-	  let allStates = Object.keys(state_json).map((key, index) =>
-			  <StateInstance full_state={state_json[key]} state={key} />
-	)
-	return (
-	  <div className="App">
-	  <br />
-	  <br />
-	  <div style={styles.root}>
-	  <GridList>
-		  {allStates}
-	  </GridList>
-	  </div>
-	  </div>
-	);
+  render () {
+    let allStates = Object.keys(stateJSON).map((key, index) =>
+      <StateInstance full_state={stateJSON[key]} state={key} />
+    )
+    return (
+      <div className="App">
+        <br />
+        <br />
+        <div style={styles.root}>
+          <GridList>
+            {allStates}
+          </GridList>
+        </div>
+      </div>
+    )
   }
 }
--- idb/src/js/Districts/District.js
+++ idb/src/js/Districts/District.js
@@ -1,107 +1,110 @@
-import '../../assets/css/App.css';
-import '../../assets/css/District.css';
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom'
+/* eslint-disable no-unused-vars */
+import React, {Component} from 'react'
+import {Link} from 'react-router-dom'
+/* eslint-enable no-unused-vars */
+import '../../assets/css/App.css'
+import '../../assets/css/District.css'
 
-let state_json = require('../../assets/data/state.json')
+let stateJSON = require('../../assets/data/state.json')
 export default class Districts extends Component {
-	constructor(props){
-		super(props)
-		this.state = {
-			districts : 0,
-			senator_count: 0,
-			total_reps: 0,
-			district_arr: [],
-			state_data: []
-		}
+  constructor (props) {
+    super(props)
+    this.state = {
+      districts: 0,
+      senator_count: 0,
+      total_reps: 0,
+      district_arr: [],
+      state_data: []
+    }
+  }
+  componentWillMount () {
+    let censusJSON = require('../../assets/data/census_data.json')
+    let repJSON = require('../../assets/data/rep_data/' +
+        this.props.match.params.districtid + '.json')
+    // let senatorJSON = require('../../assets/data/senate_data/' +
+    //   this.props.match.params.districtid + '.json')
+    let reps = this.state.total_reps
+    let districtsList = []
+    if (repJSON['status'] === 'OK') {
+      reps += repJSON['results'].length
+      for (let i = 0; i < repJSON.results.length; i++) {
+        let result = repJSON.results[i]
+        let party = 'Democratic'
+        let cssColor = 'light-blue'
+        if (result['party'] === 'R') {
+          party = 'Republican'
+          cssColor = 'light-red'
+        } else if (result['party'] === 'L') {
+          party = 'Libertarian'
+          cssColor = 'light-yellow'
+        }
 
-	}
-	componentWillMount(){
-		let census_json = require('../../assets/data/census_data.json')
-		let rep_json = require('../../assets/data/rep_data/' + this.props.match.params.districtid + '.json')
-		let senator_json = require('../../assets/data/senate_data/' + this.props.match.params.districtid + '.json')
-		let reps = this.state.total_reps
-		var districts_list = []
-		if(rep_json["status"] === "OK"){
-			reps += rep_json["results"].length
-			for (var i = 0; i < rep_json.results.length; i++) {
-				var result = rep_json.results[i]
-				var party = "Democratic"
-				var cssColor = "light-blue"
-				if (result["party"] === "R") {
-					party = "Republican"
-					cssColor = "light-red"
-				} else if (result["party"] === "L") {
-					party = "Libertarian"
-					cssColor = "light-yellow"
-				}
+        let name = 'District ' + result['district']
+        // Re-add for At-Large Districts
+        // if (repJSON.results.length === 1) {
+        //   name = result['district'] + ' District'
+        // }
 
-				var name = "District " + result["district"]
-				// Re-add for At-Large Districts
-				// if (rep_json.results.length === 1) {
-				// 	name = result["district"] + " District"
-				// }
+        console.log(censusJSON)
+        console.log('2 ' + this.props.match.params.districtid)
+        console.log(result['district'])
+        let state = censusJSON[this.props.match.params.districtid]
+        let population = state[result['district']]['population']['total']
 
-				console.log(census_json)
-				console.log("2 " + this.props.match.params.districtid)
-				console.log(result["district"])
-				var population = census_json[this.props.match.params.districtid][result["district"]]["population"]["total"]
+        districtsList.push({'district': result['district'],
+          'name': name,
+          'population': population,
+          'party': party,
+          'rep': result['name'],
+          'id': result['id'],
+          'cssColor': cssColor,
+          'rep_id': result['id']})
+      }
 
-				districts_list.push({"district": result["district"],
-									 "name": name,
-									 "population": population,
-									 "party": party,
-									 "rep": result["name"],
-									 "id": result["id"],
-									 "cssColor": cssColor,
-									 "rep_id": result["id"]})
-			}
+      districtsList.sort(function (a, b) {
+        return parseInt(a.district) - parseInt(b.district)
+      })
+      this.setState({districts: repJSON['results'].length,
+        total_reps: reps,
+        districts_arr: districtsList})
+    }
+  }
+  render () {
+    let styles = {
+      head: {
+        paddingTop: '70px'
+      },
+      imgStyle: {
+        width: '50%'
+      }
+    }
 
-            districts_list.sort(function(a, b) {
-                return parseInt(a.district) - parseInt(b.district)
-            })
-			this.setState({districts: rep_json["results"].length,
-						   total_reps: reps,
-						   districts_arr: districts_list})
-		}
-	}
-  render() {
+    let districtsGrid = this.state.districts_arr.map((district) =>
+      <div className='col-sm-3 district-grid' key={district.district}>
+        <Link to={`/districts/${this.props.match.params.districtid}/` +
+          `${district.district}`}>
+          <div className={'district-card ' + district.cssColor}>
+            <h3><b>{district.name}</b></h3>
+            <h5><b>Population: </b>{district.population}</h5>
+            <br></br>
+            <h4><b>Representative:</b></h4>
+            <h4>{district.rep}</h4>
+            <br /> <br />
+            <h4>Party: {district.party}</h4>
+          </div>
+        </Link>
+      </div>
+    )
 
-	let styles = {
-		head: {
-			paddingTop: "70px",
-		},
-		imgStyle: {
-			width: "50%"
-		}
-	}
-
-	let districts_grid = this.state.districts_arr.map(district =>
-		<div className="col-sm-3 district-grid" key={district.district}>
-			<Link to={`/districts/${this.props.match.params.districtid}/${district.district}`}>
-				<div className={"district-card " + district.cssColor}>
-					<h3><b>{district.name}</b></h3>
-					<h5><b>Population: </b>{district.population}</h5>
-					<br></br>
-					<h4><b>Representative:</b></h4>
-					<h4>{district.rep}</h4>
-					<br /> <br />
-					<h4>Party: {district.party}</h4>
-				</div>
-			</Link>
-		</div>
-	)
-
-	return (
-		<div className="container" style={styles.head}>
-			<h1 className="district-header">
-				{state_json[this.props.match.params.districtid]}
-			</h1>
-			<div className="row">
-				{districts_grid}
-			</div>
-
-		</div>
-	);
+    return (
+      <div className='container' style={styles.head}>
+        <h1 className='district-header'>
+          {stateJSON[this.props.match.params.districtid]}
+        </h1>
+        <div className='row'>
+          {districtsGrid}
+        </div>
+      </div>
+    )
   }
 }
