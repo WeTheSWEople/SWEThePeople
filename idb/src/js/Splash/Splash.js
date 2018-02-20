007e78ad2607d2e956209b530b24516999797835
Michael Tirtowidjojo <mtirtowidjojo@gmail.com>
2018-02-20 16:37:07 -0600

    Clean up splash references #96

--- idb/src/js/Splash/Splash.js
+++ idb/src/js/Splash/Splash.js
@@ -1,15 +1,18 @@
-import '../../assets/css/App.css';
+/* eslint-disable no-unused-vars */
+import React, {Component} from 'react'
+import Slider from 'react-slick'
+/* eslint-enable no-unused-vars */
+
+import '../../assets/css/App.css'
 import pic1 from '../../assets/images/splash-screen/pic1.jpg'
 import pic2 from '../../assets/images/splash-screen/pic2.jpg'
 import pic3 from '../../assets/images/splash-screen/pic3.jpg'
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-import React, { Component } from 'react';
-import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css'
+import 'slick-carousel/slick/slick-theme.css'
 
 export default class Splash extends Component {
-  constructor(props) {
-    super(props);
+  constructor (props) {
+    super(props)
 
     this.settings = {
       dots: true,
@@ -24,22 +27,22 @@ export default class Splash extends Component {
       pauseOnHover: true
     }
   }
-  render() {
-    var pics = [pic1, pic2, pic3]
+
+  render () {
+    let pics = [pic1, pic2, pic3]
 
     return (
-      <div className="App">
-      <header className="App-header">
-      </header>
-      <div className="carousel-parent">
-          <center><Slider {...this.settings} className="carousel">
-
-          {pics.map((item) => (
-              <div><img src={item} alt=""/></div>
-          ))}
-
-          </Slider></center>
-      </div>
+      <div className='App'>
+        <header className='App-header'></header>
+        <div className='carousel-parent'>
+          <center>
+            <Slider {...this.settings} className='carousel'>
+              {pics.map((item) => (
+                <div><img src={item} alt=''/></div>
+              ))}
+            </Slider>
+          </center>
+        </div>
       </div>
     )
   }
