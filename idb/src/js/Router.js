0a23f2f064c10f66dc281e60d756c73a1b11af5e
Michael Tirtowidjojo <mtirtowidjojo@gmail.com>
2018-02-20 16:01:54 -0600

    Clean up about pages references #96

--- idb/src/js/Router.js
+++ idb/src/js/Router.js
@@ -1,3 +1,8 @@
+/* eslint-disable no-unused-vars */
+import React, {Component} from 'react'
+import {Route, Switch} from 'react-router-dom'
+/* eslint-enable no-unused-vars */
+
 import About from './About/About'
 import AllDistricts from './Districts/AllDistricts'
 import Districts from './Districts/District'
@@ -7,24 +12,23 @@ import PoliticalPartyDetails from './Parties/PoliticalPartyDetails'
 import Representatives from './Representatives/Representatives'
 import RepresentativeDetails from './Representatives/RepresentativeDetails'
 import Splash from './Splash/Splash'
-import React, { Component } from 'react'
-import {Route, Switch} from 'react-router-dom'
-
 
 export default class DefaultRouter extends Component {
-  render() {
-	return (
-		<Switch>
-		  <Route exact path="/" component={Splash}/>
-			<Route path="/about" component={About}/>
-      <Route exact path="/representatives" component={Representatives}/>
-			  <Route path="/representatives/:bioguideid" component={RepresentativeDetails}/>
-		  <Route exact path="/districts" component={AllDistricts}/>
-			  <Route exact path="/districts/:districtid" component={Districts}/>
-			  <Route path="/districts/:districtid/:districtnum" component={DistrictsDetails}/>
-		  <Route path="/parties" component={PoliticalParty} />
-			  <Route path="/party/:name" component={PoliticalPartyDetails} />
-		</Switch>
-	);
+  render () {
+    return (
+      <Switch>
+        <Route exact path="/" component={Splash}/>
+        <Route path="/about" component={About}/>
+        <Route exact path="/representatives" component={Representatives}/>
+        <Route path="/representatives/:bioguideid"
+          component={RepresentativeDetails}/>
+        <Route exact path="/districts" component={AllDistricts}/>
+        <Route exact path="/districts/:districtid" component={Districts}/>
+        <Route path="/districts/:districtid/:districtnum"
+          component={DistrictsDetails}/>
+        <Route path="/parties" component={PoliticalParty} />
+        <Route path="/party/:name" component={PoliticalPartyDetails} />
+      </Switch>
+    )
   }
 }
