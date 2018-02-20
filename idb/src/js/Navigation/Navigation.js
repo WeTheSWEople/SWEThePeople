4d9e56edf239ac66c330ebdc0bea5ede02df1f88
Michael Tirtowidjojo <mtirtowidjojo@gmail.com>
2018-02-20 16:07:14 -0600

    Clean up navigation references #96

--- idb/src/js/Navigation/Navigation.js
+++ idb/src/js/Navigation/Navigation.js
@@ -1,42 +1,42 @@
-import React, { Component } from 'react';
-import { Navbar, Nav, NavItem } from 'react-bootstrap';
-import { LinkContainer } from 'react-router-bootstrap'
+/* eslint-disable no-unused-vars */
+import React, {Component} from 'react'
+import {Navbar, Nav, NavItem} from 'react-bootstrap'
+import {LinkContainer} from 'react-router-bootstrap'
+/* eslint-enable no-unused-vars */
 
 import header from '../../assets/images/header.png'
-import '../../assets/css/App.css';
+import '../../assets/css/App.css'
 
 export default class Navigation extends Component {
-    render() {
-	    return (
-	        <div>
-		        <Navbar fixedTop={true} inverse={true}>
-		            <Navbar.Header>
-			            <Navbar.Brand>
-			                <LinkContainer to="/">
-				                <img src={header}
-                                     className="App-logo"
-                                     alt="logo" />
-			                </LinkContainer>
-			            </Navbar.Brand>
-                        <Navbar.Toggle />
-		            </Navbar.Header>
-                    <Navbar.Collapse>
-                        <Nav>
-                            <LinkContainer to="/" exact={true}>
-                                <NavItem>Home</NavItem>
-                            </LinkContainer>
-                            <LinkContainer to="/representatives">
-                                <NavItem>Representatives</NavItem>
-                            </LinkContainer>
-                            <LinkContainer to="/parties">
-                                <NavItem>Parties</NavItem>
-                            </LinkContainer>
-                            <LinkContainer to="/districts">
-                                <NavItem>Districts</NavItem>
-                            </LinkContainer>
-                            <LinkContainer to="/about">
-                                <NavItem>About</NavItem>
-                            </LinkContainer>
+  render () {
+    return (
+      <div>
+        <Navbar fixedTop={true} inverse={true}>
+          <Navbar.Header>
+            <Navbar.Brand>
+              <LinkContainer to="/">
+                <img src={header} className="App-logo" alt="logo" />
+              </LinkContainer>
+            </Navbar.Brand>
+            <Navbar.Toggle />
+          </Navbar.Header>
+          <Navbar.Collapse>
+            <Nav>
+              <LinkContainer to="/" exact={true}>
+                <NavItem>Home</NavItem>
+              </LinkContainer>
+              <LinkContainer to="/representatives">
+                <NavItem>Representatives</NavItem>
+              </LinkContainer>
+              <LinkContainer to="/parties">
+                <NavItem>Parties</NavItem>
+              </LinkContainer>
+              <LinkContainer to="/districts">
+                <NavItem>Districts</NavItem>
+              </LinkContainer>
+              <LinkContainer to="/about">
+                <NavItem>About</NavItem>
+              </LinkContainer>
                         </Nav>
                     </Navbar.Collapse>
 		        </Navbar>
