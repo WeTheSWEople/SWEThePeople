e1f99ce98458f524789b5f14a9b62d9d1e5e96ad
Michael Tirtowidjojo <mtirtowidjojo@gmail.com>
2018-02-20 14:48:36 -0600

    Add eslint

--- idb/src/index.js
+++ idb/src/index.js
@@ -1,9 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-import { BrowserRouter } from 'react-router-dom';
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import registerServiceWorker from './registerServiceWorker'
+import { BrowserRouter } from 'react-router-dom'
 
-ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, document.getElementById('root'))
+registerServiceWorker()
