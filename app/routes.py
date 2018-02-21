35f8bb96e9ee5b483804e07f1913d26d7865c906
palakhirpara <palak_hirpara@yahoo.com>
2018-02-21 15:59:32 -0600

    Added one more endpoint for the reps

--- app/routes.py
+++ app/routes.py
@@ -2,6 +2,18 @@ from flask import jsonify, Blueprint
 from models import Representative
 rep_route = Blueprint('representative', __name__)
 
+
+def getResponse(data):
+	if data is None:
+		return None
+	else:
+		return data.format()
+
+
 @rep_route.route('/')
-def index():
-    return jsonify([rep.format() for rep in Representative.query.limit(50).all()])
+def representatives():
+    return jsonify([getResponse(rep) for rep in Representative.query.limit(50).all()])
+
+@rep_route.route('/<bioguide>')
+def representative(bioguide):
+    return jsonify(getResponse(Representative.query.filter(Representative.bioguide == bioguide).first()))
