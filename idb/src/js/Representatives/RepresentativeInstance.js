52deb9221d5d271db8a512c1f8f59e0cd13acae6
bzsinger <benjamin.z.singer@gmail.com>
2018-02-20 19:04:13 -0600

    Clean up representatives #96

--- idb/src/js/Representatives/RepresentativeInstance.js
+++ idb/src/js/Representatives/RepresentativeInstance.js
@@ -1,33 +1,44 @@
-import '../../assets/css/App.css';
-import '../../assets/css/RepresentativeInstance.css';
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom'
-import {GridTile} from 'material-ui/GridList';
+/* eslint-disable no-unused-vars */
+import React, {Component} from 'react'
+import {Link} from 'react-router-dom'
+import {GridTile} from 'material-ui/GridList'
+/* eslint-enable no-unused-vars */
+import '../../assets/css/App.css'
+import '../../assets/css/RepresentativeInstance.css'
 
 const styles = {
   hyperlink: {
-    textDecoration: "none",
-    color: "black"
-  },
-};
+    textDecoration: 'none',
+    color: 'black'
+  }
+}
 
 export default class RepresentativeInstance extends Component {
-  render() {
+  render () {
     return (
       <Link
         to={`/representatives/${this.props.rep.bioguide}`}
         style={styles.hyperlink}>
-          <GridTile
-            key={this.props.rep.bioguide}
-            class="tile" >
-            <img src={"https://theunitedstates.io/images/congress/225x275/" + this.props.rep.bioguide + ".jpg"}  alt={this.props.rep.firstName} class="rep_img" />
-            <div class="rep_info">
-            <h3 class="title">{this.props.rep.firstName + " " + this.props.rep.lastName}</h3>
-            <h4 class="party">{this.props.rep.party}</h4>
-            <h4 class="district"><i>{this.props.rep.state + " - " + this.props.rep.district}</i></h4>
-            </div>
-          </GridTile>
+        <GridTile
+          key={this.props.rep.bioguide}
+          class='tile' >
+          <img
+            src={'https://theunitedstates.io/images/congress/225x275/' +
+            this.props.rep.bioguide + '.jpg'}
+            alt={this.props.rep.firstName} class='rep_img'
+          />
+          <div class='rep_info'>
+            <h3 class='title'>
+              {this.props.rep.firstName +
+              ' ' + this.props.rep.lastName}
+            </h3>
+            <h4 class='party'>{this.props.rep.party}</h4>
+            <h4 class='district'><i>
+              {this.props.rep.state + ' - ' + this.props.rep.district}
+            </i></h4>
+          </div>
+        </GridTile>
       </Link>
-    );
+    )
   }
 }
--- idb/src/js/Representatives/Representatives.js
+++ idb/src/js/Representatives/Representatives.js
@@ -1,11 +1,11 @@
+/* eslint-disable no-unused-vars */
+import React, {Component} from 'react'
+import {GridList} from 'material-ui/GridList'
+/* eslint-disable no-unused-vars */
 import RepresentativeInstance from './RepresentativeInstance'
-import '../../assets/css/App.css';
-import React, { Component } from 'react';
-import {GridList} from 'material-ui/GridList';
+import '../../assets/css/App.css'
 
-import allReps from '../../assets/all-reps-endpoint.json';
-
-
+import allReps from '../../assets/all-reps-endpoint.json'
 
 const styles = {
   root: {
@@ -14,40 +14,37 @@ const styles = {
     paddingTop: '50px',
     paddingLeft: '50px',
     paddingRight: '50px',
-    justifyContent: 'space-around',
+    justifyContent: 'space-around'
   },
   gridList: {
     width: '100%',
     height: '100%',
-    overflowY: 'auto',
-  },
-};
-
+    overflowY: 'auto'
+  }
+}
 
 export default class Representatives extends Component {
-  constructor(props) {
-    super(props);
+  constructor (props) {
+    super(props)
 
     this.state = allReps
-
   }
-  render() {
+  render () {
     return (
-      <div className="App">
-      <header className="App-header-white">
-      </header>
-      <div style={styles.root}>
-      <GridList
-        cellHeight={400}
-        cols={5}
-        style={styles.gridList}
-      >
-        {this.state.map((item) => (
-            <RepresentativeInstance key={item.bioguide} rep = {item} />
-        ))}
-      </GridList>
+      <div className='App'>
+        <header className='App-header-white'></header>
+        <div style={styles.root}>
+          <GridList
+            cellHeight={400}
+            cols={5}
+            style={styles.gridList}
+          >
+            {this.state.map((item) => (
+              <RepresentativeInstance key={item.bioguide} rep = {item} />
+            ))}
+          </GridList>
+        </div>
       </div>
-      </div>
-    );
+    )
   }
 }
