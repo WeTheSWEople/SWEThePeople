d0e33bb444a94521726d2a02e55eea9410d5b2cf
bzsinger <benjamin.z.singer@gmail.com>
2018-02-22 17:57:31 -0600

    Began representatives scraper

---
+++ representatives_scraper.py
@@ -0,0 +1,15 @@
+import requests
+import json
+import os
+import apikeys
+
+CURRENT_CONGRESS = 115
+API_KEY = 'icU6XnQ63Mu9qDhEg1QCz0Emb7wt5n9GoLEAEnmI'
+
+url = 'https://api.propublica.org/congress/v1/' + str(CURRENT_CONGRESS) + '/house/members.json'
+
+headers = {
+	'x-api-key': API_KEY,
+}
+
+response = requests.request('GET', url, headers=headers) with open(os.path.join('./idb/src/assets/data/representatives.json'), 'w') as file1: file1.write(response.text.encode('utf-8'))
