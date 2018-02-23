9d4d3f3d9d1b14c0cec7d267ba1b954fe757f798
Minwoo Jo <minwoo0jo@hotmail.com>
2018-02-23 12:57:12 -0600

    Now ignores out of office reps and scraper no longer crashes upon finding a duplicate

--- representatives_scraper.py
+++ representatives_scraper.py
@@ -35,8 +35,23 @@ for mem in members['results'][0]['members']:
 			url = mem['url'],
 			image_uri = 'https://theunitedstates.io/images/congress/225x275/' + mem['id'] +'.jpg'
 			)
-	db.session.add(rep)
-	db.session.commit()
+		rep2 = Representative.query.filter(Representative.bioguide == rep.bioguide+"dsdf").first()
+		if rep2 == None:
+			db.session.add(rep)
+			db.session.commit()
+		else:
+			rep2.firstname = rep.firstname
+			rep2.lastname = rep.lastname
+			rep2.party = rep.party
+			rep2.state = rep.state
+			rep2.district = rep.district
+			rep2.twitter = rep.twitter
+			rep2.youtube = rep.youtube
+			rep2.votes_with_party_pct = rep.votes_with_party_pct
+			rep2.url = rep.url
+			rep2.image_uri = rep.image_uri
+			db.session.commit()
+		
 
 with open(os.path.join('./idb/src/assets/data/representatives.json'), 'w') as file1:
 	file1.write(response.text.encode('utf-8'))
