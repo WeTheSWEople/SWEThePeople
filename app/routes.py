44b5befda91c3002800d3c93c76d33778a03ff78
palakhirpara <palak_hirpara@yahoo.com>
2018-02-23 09:59:20 -0600

    Added more end points

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,9 @@
-from flask import jsonify, Blueprint
+from flask import jsonify, Blueprint, send_from_directory
 from models import Representative
+root_route = Blueprint('root', __name__)
 rep_route = Blueprint('representative', __name__)
+
+
 
 
 def getResponse(data):
@@ -9,6 +12,10 @@ def getResponse(data):
 	else:
 		return data.format()
 
+
+@root_route.route('/')
+def endpoints():
+	return send_from_directory('static', 'index.html')
 
 @rep_route.route('/')
 def representatives():
