6c0f7ba9f64d63dd99dc097aa1344718042e42d5
bzsinger <benjamin.z.singer@gmail.com>
2018-02-22 22:15:34 -0600

    Working on adding reps to database

--- representatives_scraper.py
+++ representatives_scraper.py
@@ -2,6 +2,8 @@ import requests
 import json
 import os
 #import apikeys
+from app.models import Representative, Bill
+from app.app import create_app
 
 CURRENT_CONGRESS = 115
 API_KEY = 'icU6XnQ63Mu9qDhEg1QCz0Emb7wt5n9GoLEAEnmI'
@@ -12,6 +14,16 @@ headers = {
 	'x-api-key': API_KEY,
 }
 
+app = create_app()
+app.app_context().push()
+#rep = Representative(bioguide=‘R000570’, firstname=‘Paul’, lastname=‘Ryan’, party=‘Republican’, state=‘WI’, district=1, twitter=‘speakerryan’, youtube=“reppaulryan”, office=“1233 Longhorn”, votes_with_party_pct = 85.71, url=“”,  image_uri=’www.google.com')
+
 response = requests.request('GET', url, headers=headers)
+members = response["members"]
+print(members)
+
+
 with open(os.path.join('./idb/src/assets/data/representatives.json'), 'w') as file1:
 	file1.write(response.text)
+db.session.add(rep)
+db.session.commit()
