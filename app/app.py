b38497bc4ef72f6f8f1a2f1d530398ff30fb0b2f
palakhirpara <palak_hirpara@yahoo.com>
2018-02-22 18:10:30 -0600

    fixed the endpoint issue

--- app/app.py
+++ app/app.py
@@ -8,7 +8,7 @@ def create_app():
 	app = Flask(__name__)
 	app.config.from_json(config_file)
 	from routes import rep_route
-	app.register_blueprint(rep_route, url_prefix='/representative')
+	app.register_blueprint(rep_route, url_prefix='/')
 	db.init_app(app)
 	db.create_all(app=app)
 	return app
