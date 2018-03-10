0fa5907be9ac71d1f3ecce14373915981a161097
copperstick6 <copperstick6@gmail.com>
2018-03-09 18:24:06 -0600

    added unit tests for db, api, local, live

--- router.py
+++ router.py
@@ -7,14 +7,14 @@ app = Flask(__name__, static_folder='idb/build')
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def serve(path):
-    if(path == ""):
-        return send_from_directory('idb/build', 'index.html')
-    else:
-        if(os.path.exists("idb/build/" + path)):
-            return send_from_directory('idb/build', path)
-        else:
-            return send_from_directory('idb/build', 'index.html')
+	if(path == ""):
+		return send_from_directory('idb/build', 'index.html')
+	else:
+		if(os.path.exists("idb/build/" + path)):
+			return send_from_directory('idb/build', path)
+		else:
+			return send_from_directory('idb/build', 'index.html')
 
 
 if __name__ == '__main__':
-    app.run(use_reloader=True, port=5000, threaded=True)
+	app.run(use_reloader=True, port=5000, threaded=True)
