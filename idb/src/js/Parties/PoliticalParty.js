85a08300a046f760926b6e6f82262931358f8605
Michael Tirtowidjojo <mtirtowidjojo@gmail.com>
2018-03-20 12:00:20 -0500

    Change parties index to pull from api reference #102

--- idb/src/js/Parties/PoliticalParty.js
+++ idb/src/js/Parties/PoliticalParty.js
@@ -5,8 +5,10 @@ import React, {Component} from 'react'
 
 import '../../assets/css/App.css'
 // import allParties from '../../assets/all-parties.json'
-import allParties from '../../assets/parties.json'
+// import allParties from '../../assets/parties.json'
 import repsInfo from '../../assets/all-reps-endpoint.json'
+
+let request = require('request')
 
 export default class PoliticalParty extends Component {
   constructor (props) {
@@ -22,26 +24,36 @@ export default class PoliticalParty extends Component {
   componentWillMount () {
     this.setState({ready: false})
 
-    let partiesMap = {}
-    Object.keys(allParties).forEach(function (partyName) {
-      partiesMap[partyName] = {
-        path: allParties[partyName]['path'],
-        name: allParties[partyName]['name'],
-        color: allParties[partyName]['color'],
-        chair: allParties[partyName]['chair'],
-        num_reps: 0
+    let options = {method: 'GET', url: 'http://api.swethepeople.me/party/'}
+    request(options, function (error, response, body) {
+      if (error) {
+        this.setState({error: true, ready: true})
+      } else {
+        console.log('JASDJFASFDADSFADF')
+        let allParties = JSON.parse(body)
+        let partiesMap = {}
+        Object.keys(allParties).forEach(function (partyName) {
+          partiesMap[partyName] = {
+            path: allParties[partyName]['path'],
+            name: allParties[partyName]['name'],
+            // color: allParties[partyName]['color'],
+            chair: allParties[partyName]['chair'],
+            num_reps: allParties[partyName]['representatives'].length
+          }
+        })
+
+        // Object.keys(repsInfo).forEach(function (repKey) {
+        //   Object.keys(partiesMap).forEach(function (partyKey) {
+        //     if (partyKey.startsWith(repsInfo[repKey]['party'])) {
+        //       partiesMap[partyKey]['num_reps'] += 1
+        //     }
+        //   })
+        // })
+
+        console.log(partiesMap)
+        this.setState({parties: partiesMap, ready: true})
       }
-    })
-
-    Object.keys(repsInfo).forEach(function (repKey) {
-      Object.keys(partiesMap).forEach(function (partyKey) {
-        if (partyKey.startsWith(repsInfo[repKey]['party'])) {
-          partiesMap[partyKey]['num_reps'] += 1
-        }
-      })
-    })
-
-    this.setState({parties: partiesMap, ready: true})
+    }.bind(this))
   }
 
   render () {
