59f901b5f0619b69699118fd80b2cf0026e3a398
raulcodes <rcamacho96@gmail.com>
2018-03-20 23:48:34 -0500

    Added most unit tests

--- idb/test/tests.js
+++ idb/test/tests.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { expect } from 'chai';
+import { expect, assert } from 'chai';
 import Enzyme from 'enzyme';
 import { mount, shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
@@ -10,6 +10,13 @@ import Splash from '../src/js/Splash/Splash';
 import Slider from 'react-slick';
 import Navigation from '../src/js/Navigation/Navigation';
 import { NavItem } from 'react-bootstrap';
+import RepresentativeDetails from '../src/js/Representatives/RepresentativeDetails.js';
+import RepresentativeInstance from '../src/js/Representatives/RepresentativeInstance.js';
+import allReps from '../src/assets/bioguide-endpoint.json';
+import Representatives from '../src/js/Representatives/Representatives.js';
+import StateInstance from '../src/js/Representatives/StateInstance.js';
+import PoliticalParty from '../src/js/Parties/PoliticalParty.js';
+import PoliticalPartyDetails from '../src/js/Parties/PoliticalPartyDetails.js';
 
 describe('Splash Page Component', () => {
   it('renders header', () => {
@@ -36,3 +43,68 @@ describe('Navigation Component', () => {
     expect(wrapper.find(NavItem)).to.have.length(5)
   })
 })
+
+// describe('RepresentativeDetails Component', () => {
+//   it('renders bill mappings', () => {
+//     const wrapper = shallow(<RepresentativeDetails />)
+//     // expect(wrapper.find()
+//   })
+// })
+
+describe('RepresentativeInstance Component', () => {
+  const instance = shallow(<RepresentativeInstance rep={allReps['R000570']}/>)
+
+  it('displays correct picture of representative', () => {
+    const { alt, src } = instance.find('img').props()
+    assert.equal(instance.find('img').length, 1)
+    assert.equal(alt, "Paul")
+    assert.equal(src, "https://theunitedstates.io/images/congress/225x275/R000570.jpg")
+  })
+  it('displays representative name and info', () => {
+    expect(instance.find('.title').text()).to.equal("Paul Ryan")
+    expect(instance.find('.party').text()).to.equal("Republican")
+    expect(instance.find('.district').text()).to.equal("WI - 1")
+  })
+})
+
+describe('Representatives Component', () => {
+  const representatives = shallow(<Representatives />)
+
+  it('displays a grid of representatives', () => {
+    expect(representatives.find('div.grid-container').children()).to.have.length(1)
+    expect(representatives.find('.gridlist-container').children()).to.have.length(4)
+  })
+})
+
+describe('StateInstance Component', () => {
+  const stateInstance = shallow(<StateInstance full_state={"California"} state={"CA"}/>)
+
+  it('displays correct picture of state', () => {
+    const { alt, src } = stateInstance.find('img').props()
+    assert.equal(stateInstance.find('img').length, 1)
+    assert.equal(alt, "State")
+  })
+
+  it('displays correct state info', () => {
+    expect(stateInstance.find('h3.title').text()).to.equal("California")
+    expect(stateInstance.find('h4.party').text()).to.equal("Districts: 2")
+    expect(stateInstance.find('h4.district').text()).to.equal("Senators: 2")
+  })
+})
+
+describe('PoliticalParty Component', () => {
+  const politicalpartyInstance = shallow(<PoliticalParty />)
+
+  it('displays grid of political parties', () => {
+    expect(politicalpartyInstance.find('div.parties-container').children()).to.have.length(1)
+  })
+})
+
+// describe('PoliticalPartyDetails Component', () => {
+//   const politicalpartydetailsInstance = shallow(<PoliticalPartyDetails />)
+
+//   it('displays details of political party', () => {
+//     expect(politicalpartydetailsInstance.find('div.party-info').children()).to.have.length(1)
+//   })
+// })
+
