d9c18164d4b3670342bae6a0243e10f119176bec
copperstick6 <copperstick6@gmail.com>
2018-03-27 19:25:36 -0500

    added basic search components, unstyled search boxes

--- frontend/src/js/Parties/PoliticalParty.js
+++ frontend/src/js/Parties/PoliticalParty.js
@@ -10,70 +10,91 @@ let request = require('request')
 
 const styles = {
   center:{
-    display: 'flex',
-    flexWrap: 'wrap',
-    paddingTop: '25%',
-    paddingLeft: '50px',
-    paddingRight: '50px',
-    justifyContent: 'space-around'
+	display: 'flex',
+	flexWrap: 'wrap',
+	paddingTop: '25%',
+	paddingLeft: '50px',
+	paddingRight: '50px',
+	justifyContent: 'space-around'
   }
 }
 export default class PoliticalParty extends Component {
-  constructor (props) {
-    super(props)
+	constructor (props) {
+	  super(props)
 
-    this.state = {
-      ready: false,
-      error: false,
-      parties: {}
-    }
-  }
+	  this.state = {
+		ready: false,
+		error: false,
+		value: "",
+		parties: {},
+		all_parties: {}
+	  }
+	  this.handleChange = this.handleChange.bind(this)
+	}
+	handleChange(event){
+		this.setState({value: event.target.value})
+		if (event.target.value.length == 0){
+			this.setState({parties:this.state.all_parties})
+		}
+		else{
+			let reduced_dict = {}
+			let index = 0
+			for (var item in this.state.all_parties){
+				if (this.state.all_parties[item]["name"].toLowerCase().includes(event.target.value.toLowerCase())){
+					reduced_dict[index] = this.state.all_parties[item]
+					index++
+				}
+			}
+			this.setState({parties: reduced_dict})
+		}
+	}
 
-  componentWillMount () {
-    this.setState({ready: false})
+	componentWillMount () {
+	  this.setState({ready: false})
 
-    let options = {method: 'GET', url: 'http://api.swethepeople.me/party/'}
-    request(options, function (error, response, body) {
-      if (error) {
-        this.setState({error: true, ready: true})
-      } else {
-        let allParties = JSON.parse(body)
-        let partiesMap = {}
-        Object.keys(allParties).forEach(function (partyName) {
-          partiesMap[partyName] = {
-            path: allParties[partyName]['path'],
-            name: allParties[partyName]['name'],
-            chair: allParties[partyName]['chair'],
-            num_reps: allParties[partyName]['representatives'].length
-          }
-        })
+	  let options = {method: 'GET', url: 'http://api.swethepeople.me/party/'}
+	  request(options, function (error, response, body) {
+		if (error) {
+		  this.setState({error: true, ready: true})
+		} else {
+		  let allParties = JSON.parse(body)
+		  let partiesMap = {}
+		  Object.keys(allParties).forEach(function (partyName) {
+			partiesMap[partyName] = {
+			  path: allParties[partyName]['path'],
+			  name: allParties[partyName]['name'],
+			  chair: allParties[partyName]['chair'],
+			  num_reps: allParties[partyName]['representatives'].length
+			}
+		  })
 
-        this.setState({parties: partiesMap, ready: true})
-      }
-    }.bind(this))
-  }
+		  this.setState({parties: partiesMap, all_parties: partiesMap, ready: true})
+		}
+	  }.bind(this))
+	}
 
   render () {
-    let parties = null
-    if (this.state.ready) {
-      let divStyle = {
-        paddingTop: '70px'
-      }
-      parties = <PoliticalPartyInstance party_data = {this.state.parties} />
+	let parties = null
+	if (this.state.ready) {
+	  let divStyle = {
+		paddingTop: '70px'
+	  }
+	  parties = <PoliticalPartyInstance party_data = {this.state.parties} />
 
-      return (
-        <div style={divStyle} className="parties-container">
-          {parties}
-        </div>
-      )
-    }
-    else {
+	  return (
+		<div style={divStyle} className="parties-container">
+		<center><input type="text" value={this.state.value} onChange={this.handleChange} /></center>
+		  {parties}
+		</div>
+	  )
+	}
+	else {
 
-      return(
-      <div style={styles.center}>
-      <RingLoader color={'#123abc'} loading={true} />
-       </div>)
+	  return(
+	  <div style={styles.center}>
+	  <RingLoader color={'#123abc'} loading={true} />
+	   </div>)
 
-    }
+	}
   }
 }
--- frontend/src/js/Representatives/Representatives.js
+++ frontend/src/js/Representatives/Representatives.js
@@ -10,99 +10,121 @@ import axios from 'axios'
 
 const styles = {
   root: {
-    display: 'flex',
-    flexWrap: 'wrap',
-    paddingTop: '50px',
-    paddingLeft: '50px',
-    paddingRight: '50px',
-    justifyContent: 'space-around'
+	display: 'flex',
+	flexWrap: 'wrap',
+	paddingTop: '50px',
+	paddingLeft: '50px',
+	paddingRight: '50px',
+	justifyContent: 'space-around'
   },
   center:{
-    display: 'flex',
-    flexWrap: 'wrap',
-    paddingTop: '25%',
-    paddingLeft: '50px',
-    paddingRight: '50px',
-    justifyContent: 'space-around'
+	display: 'flex',
+	flexWrap: 'wrap',
+	paddingTop: '25%',
+	paddingLeft: '50px',
+	paddingRight: '50px',
+	justifyContent: 'space-around'
   },
   gridList: {
-    width: '100%',
-    height: '100%',
-    overflowY: 'auto'
+	width: '100%',
+	height: '100%',
+	overflowY: 'auto'
   }
 }
 
 export default class Representatives extends Component {
-  constructor (props) {
-    super(props)
-    this.state = {
-        all_reps : null,
-        party_name : null
-    };
-  }
+	constructor (props) {
+	  super(props)
+	  this.state = {
+		  reps : null,
+		  all_reps: null,
+		  party_name : null,
+		  value: ""
+	  };
+	  this.handleChange = this.handleChange.bind(this)
+	}
+
+	handleChange(event){
+		this.setState({value: event.target.value})
+		if (event.target.value.length == 0){
+			this.setState({reps:this.state.all_reps})
+		}
+		else{
+			let reduced_dict = {}
+			let reps_arr = []
+			let index = 0
+			for (var item in this.state.all_reps){
+				if (this.state.all_reps[item]["firstname"].toLowerCase().includes(event.target.value.toLowerCase()) || this.state.all_reps[item]["lastname"].toLowerCase().includes(event.target.value.toLowerCase())){
+					reduced_dict[index] = this.state.all_reps[item]
+					reps_arr.push(reduced_dict[index])
+					index++
+				}
+			}
+			this.setState({reps: reps_arr})
+		}
+	}
 
 
-  componentDidMount(){
+	componentDidMount(){
 
-    // get the reps data
-    axios.get(`http://api.swethepeople.me/representative/`)
-    .then((response)=>{
-      this.setState({
-        all_reps:response.data
-      })
-      // get the party names
-      return axios.get(`http://api.swethepeople.me/party?party_name=True`)
-    })
-    .then((response)=>{
-      this.setState({
-        party_name:response.data
-      })
-    })
-    .catch((error)=>{
-      this.setState({
-          all_reps: -1,
-          party_name: -1
+	  // get the reps data
+	  axios.get(`http://api.swethepeople.me/representative/`)
+	  .then((response)=>{
+		this.setState({
+		  reps:response.data,
+		  all_reps: response.data
+		})
+		// get the party names
+		return axios.get(`http://api.swethepeople.me/party?party_name=True`)
+	  })
+	  .then((response)=>{
+		this.setState({
+		  party_name:response.data
+		})
+	  })
+	  .catch((error)=>{
+		this.setState({
+			reps: -1,
+			all_reps: -1,
+			party_name: -1
 
-      })
-    })
-
-
-
-
-  }
+		})
+	  })
+	}
 
   render () {
-    if (this.state.all_reps === null || this.state.party_name === null){
-      return(
-      <div style={styles.center} className="loading">
-      <RingLoader color={'#123abc'} loading={true} />
-       </div>)
-    }
-    else if (this.state.all_reps === -1  || this.state.party_name === -1){
-      return (
-          <div style={styles.root}>
-           <p> Data Not Found </p>
-          </div>)
-    }
-    else{
-      return (
-        <div className='App'>
-          <header className='App-header-white'></header>
-          <div style={styles.root} className="grid-container">
-            <GridList
-              cellHeight={400}
-              cols={5}
-              style={styles.gridList}
-              className="gridlist-container"
-            >
-              {this.state.all_reps.map((item) => (
-                <RepresentativeInstance key={item.bioguide} rep = {item} party_name = {this.state.party_name[item.party_id][0]} />
-              ))}
-            </GridList>
-          </div>
-        </div>
-      )
-    }
+	if (this.state.all_reps === null || this.state.party_name === null){
+	  return(
+	  <div style={styles.center} className="loading">
+	  <RingLoader color={'#123abc'} loading={true} />
+	   </div>)
+	}
+	else if (this.state.all_reps === -1  || this.state.party_name === -1){
+	  return (
+		  <div style={styles.root}>
+		   <p> Data Not Found </p>
+		  </div>)
+	}
+	else{
+	  return (
+		<div className='App'>
+		  <header className='App-header-white'></header>
+		  <div style={styles.root} className="grid-container">
+		  <center><input type="text" value={this.state.value} onChange={this.handleChange} /></center>
+			<GridList
+			  cellHeight={400}
+			  cols={5}
+			  style={styles.gridList}
+			  className="gridlist-container"
+			>
+			  {this.state.reps.map((item) => (
+				<RepresentativeInstance key={item.bioguide} rep = {item} party_name = {this.state.party_name[item.party_id][0]} />
+			  ))}
+			</GridList>
+		  </div>
+		</div>
+	  )
+	}
 
   }
 }
