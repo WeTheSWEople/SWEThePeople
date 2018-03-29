19693df424ac2610a19878a56d76737feed1c3aa
palakhirpara <palak_hirpara@yahoo.com>
2018-03-29 11:10:59 -0500

    adding searching functionality to the models

--- backend/routes.py
+++ backend/routes.py
@@ -1,12 +1,14 @@
 from flask import jsonify, Blueprint, send_from_directory, render_template, request
 from models import Representative, PoliticalParty, PartyColor, District, State
 from util import *
+from sqlalchemy.orm import load_only, defer
 root_route = Blueprint('root', __name__)
 rep_route = Blueprint('representative', __name__)
 error_route = Blueprint('error', __name__)
 party_route = Blueprint('political_party', __name__)
 state_route = Blueprint('state', __name__)
 district_route = Blueprint('district', __name__)
+search_route = Blueprint('search', __name__)
 
 # def get_response(data):
 #     if data is None:
@@ -83,6 +85,106 @@ def districts_by_id(abbrev, id):
     if not data:
         return error("Item not found for id " + abbrev + " and " + id)
     return jsonify(get_response(data))
+
+@search_route.route("/")
+def search():
+    search_query = request.args.get('query')
+    print(search_query)
+    reps_result = []
+    parties_result = []
+    districts_result = []
+
+    # data = model.query.filter(model_id == parameter).first()
+    # if not data:
+    #     return error("Item not found for id " + parameter)
+    # return jsonify(get_response(data))
+
+#with_entities(PoliticalParty.chair, PoliticalParty.colors, PoliticalParty.formation_date, PoliticalParty.name, PoliticalParty.office, PoliticalParty.path).
+
+    reps = Representative.query.search(search_query).all()
+    if reps:
+        for rep in reps:
+            item = get_response(rep)
+            del item['bills']
+            reps_result.append(item)
+        #reps_result += [del get_response(rep) ['bills'] for rep in reps]
+        # add the corresponding party stuff
+        for rep in reps:
+            print(rep.party_id)
+            party = PoliticalParty.query.with_entities(PoliticalParty.id, PoliticalParty.name, \
+                    PoliticalParty.chair, \
+                    PoliticalParty.formation_date, \
+                    PoliticalParty.office, \
+                    PoliticalParty.path)\
+                    .filter(PoliticalParty.id == rep.party_id).first()
+            party_json = {
+                "id": party.id,
+                "name": party.name,
+                "path": party.path,
+                "chair": party.chair,
+                "formation_date": party.formation_date,
+                "office": party.office,
+                "path": party.path
+            }  
+
+            if party_json not in parties_result:
+                parties_result.append(party_json)
+
+            district = District.query.filter(District.representative_id == rep.bioguide).first()
+            district_json = {
+                "alpha_num": district.alpha_num,
+                "id": district.id,
+                "representative_id": district.representative_id,
+                "state": district.state
+            }            
+            if district_json not in districts_result:
+                districts_result.append(district_json)        
+
+        # add the corresponding district stuff
+
+
+    parties = PoliticalParty.query.search(search_query).all()
+    if parties:
+        for party in parties:
+            item = get_response(party)
+            del item['representatives']
+            del item['colors']
+            del item['twitter_handle']
+            del item['website']
+            del item['youtube']
+            parties_result.append(item)
+
+        for party in parties:
+            reps = Representative.query.filter(party.id == Representative.party_id).all()
+            for rep in reps:
+                rep_json = get_response(rep)
+                del rep_json['bills']
+                if rep_json not in reps_result:
+                    reps_result.append(rep_json)
+
+                district = District.query.filter(District.representative_id == rep.bioguide).first()
+                district_json = {
+                    "alpha_num": district.alpha_num,
+                    "id": district.id,
+                    "representative_id": district.representative_id,
+                    "state": district.state
+                }            
+                if district_json not in districts_result:
+                    districts_result.append(district_json) 
+
+                   
+    #districts = District.query.search(search_query).all()
+
+    return jsonify({
+        "reps": reps_result,
+        "parties": parties_result,
+        "districts": districts_result
+    })
+    # parties = 
+    # districts = 
+
+
+
 
 @error_route.app_errorhandler(404)
 def url_not_found(e):
