66e1cb87609f4740cc0ae76d538f0119220dcf04
Michael Tirtowidjojo <mtirtowidjojo@gmail.com>
2018-04-02 19:09:50 -0500

    Clean up filter components

--- frontend/src/js/Districts/DistrictFilter.js
+++ frontend/src/js/Districts/DistrictFilter.js
@@ -9,7 +9,7 @@ import '../../assets/css/Filter.css'
 import 'react-select/dist/react-select.css'
 
 export default class DistrictFilter extends Component {
-  constructor(props) {
+  constructor (props) {
     super(props)
 
     let stateArr = []
@@ -28,30 +28,32 @@ export default class DistrictFilter extends Component {
     }
 
     this.handleStateDropdownChange = this.handleStateDropdownChange.bind(this)
-    this.handlePopluationDropdownChange = this.handlePopluationDropdownChange.bind(this)
-    this.handleMedianAgeDropdownChange = this.handleMedianAgeDropdownChange.bind(this)
+    this.handlePopluationDropdownChange = this.handlePopluationDropdownChange
+      .bind(this)
+    this.handleMedianAgeDropdownChange = this.handleMedianAgeDropdownChange
+      .bind(this)
     this.handleSortDropdownChange = this.handleSortDropdownChange.bind(this)
     this.handleFilterClicked = this.handleFilterClicked.bind(this)
     this.handleResetClicked = this.handleResetClicked.bind(this)
   }
 
-  handleStateDropdownChange(selectedOption) {
+  handleStateDropdownChange (selectedOption) {
     this.setState({state_value: selectedOption})
   }
 
-  handlePopluationDropdownChange(selectedOption) {
+  handlePopluationDropdownChange (selectedOption) {
     this.setState({population_value: selectedOption})
   }
 
-  handleMedianAgeDropdownChange(selectedOption) {
+  handleMedianAgeDropdownChange (selectedOption) {
     this.setState({median_age_value: selectedOption})
   }
 
-  handleSortDropdownChange(selectedOption) {
+  handleSortDropdownChange (selectedOption) {
     this.setState({sort_value: selectedOption})
   }
 
-  handleFilterClicked(e) {
+  handleFilterClicked (e) {
     let state = 'None'
     if (this.state.state_value !== null) {
       state = this.state.state_value.value
@@ -62,9 +64,9 @@ export default class DistrictFilter extends Component {
       population = this.state.population_value.value
     }
 
-    let median_age = 'None'
+    let medianAge = 'None'
     if (this.state.median_age_value !== null) {
-      median_age = this.state.median_age_value.value
+      medianAge = this.state.median_age_value.value
     }
 
     let sort = 'state_asc'
@@ -72,20 +74,20 @@ export default class DistrictFilter extends Component {
       sort = this.state.sort_value.value
     }
 
-    this.props.buttonHandler(state, population, median_age, sort)
+    this.props.buttonHandler(state, population, medianAge, sort)
   }
 
-  handleResetClicked(e) {
+  handleResetClicked (e) {
     this.setState({
       state_value: null,
       population_value: null,
       median_age_value: null,
-      sort_value: "state_asc"
+      sort_value: 'state_asc'
     })
     this.props.buttonHandler('None', 'None', 'None', 'state_asc')
   }
 
-  render() {
+  render () {
     return (
       <div style={{marginLeft: '6%', marginRight: '5%', width: '90%'}}>
         <div className='filter-component row'>
@@ -104,10 +106,10 @@ export default class DistrictFilter extends Component {
               value={this.state.population_value}
               onChange={this.handlePopluationDropdownChange}
               options={[{value: '0-250000', label: '< 250,000'},
-                        {value: '250000-500000', label: '250,000 - 499,999'},
-                        {value: '500000-750000', label: '500,000 - 749,999'},
-                        {value: '750000-1000000', label: '750,000 - 999,999'},
-                        {value: '1000000-10000000', label: '>= 1,000,000'}]}
+                {value: '250000-500000', label: '250,000 - 499,999'},
+                {value: '500000-750000', label: '500,000 - 749,999'},
+                {value: '750000-1000000', label: '750,000 - 999,999'},
+                {value: '1000000-10000000', label: '>= 1,000,000'}]}
             />
           </div>
 
@@ -117,10 +119,10 @@ export default class DistrictFilter extends Component {
               value={this.state.median_age_value}
               onChange={this.handleMedianAgeDropdownChange}
               options={[{value: '0-30', label: '<= 29'},
-                        {value: '30-40', label: '30 - 39'},
-                        {value: '40-50', label: '40 - 49'},
-                        {value: '50-60', label: '50 - 59'},
-                        {value: '60-100', label: '>= 60'}]}
+                {value: '30-40', label: '30 - 39'},
+                {value: '40-50', label: '40 - 49'},
+                {value: '50-60', label: '50 - 59'},
+                {value: '60-100', label: '>= 60'}]}
             />
           </div>
 
@@ -130,9 +132,9 @@ export default class DistrictFilter extends Component {
               value={this.state.sort_value}
               onChange={this.handleSortDropdownChange}
               options={[{value: 'state_asc', label: 'State (ASC)'},
-                        {value: 'state_desc', label: 'State (DESC)'},
-                        {value: 'population_asc', label: 'Population (ASC)'},
-                        {value: 'population_desc', label: 'Population (DESC)'}]}
+                {value: 'state_desc', label: 'State (DESC)'},
+                {value: 'population_asc', label: 'Population (ASC)'},
+                {value: 'population_desc', label: 'Population (DESC)'}]}
             />
           </div>
 
