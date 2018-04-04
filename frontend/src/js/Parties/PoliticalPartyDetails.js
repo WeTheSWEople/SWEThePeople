5f14cad7e469f907b8e5a2524311a4a8e6255438
bzsinger <benjamin.z.singer@gmail.com>
2018-04-04 17:35:10 -0500

    Fixed missing office location/YouTube/Twitter message

--- frontend/src/js/Parties/PoliticalPartyDetails.js
+++ frontend/src/js/Parties/PoliticalPartyDetails.js
@@ -186,7 +186,7 @@ export default class PoliticalPartyDetails extends Component {
       noControl = '0/' + this.state.totalReps
     }
 
-    let twitter = 'No twitter handle'
+    let twitter = <h4><b>No Twitter handle</b></h4>
     if (this.state.party != null && this.state.party['twitter_handle'] !== '') {
       twitter = <div>
         <Timeline dataSource={{sourceType: 'profile',
@@ -196,7 +196,7 @@ export default class PoliticalPartyDetails extends Component {
       </div>
     }
 
-    let youtube = 'No youtube channel'
+    let youtube = <h4><b>No YouTube channel</b></h4>
     if (this.state.party != null && this.state.party['youtube'] !== '') {
       youtube = <div className='youtube-div'>
         <h4><b>YouTube Channel</b></h4>
@@ -212,7 +212,7 @@ export default class PoliticalPartyDetails extends Component {
       </div>
     }
 
-    let office = 'No office location'
+    let office = <h4><b>No office location</b></h4>
     if (this.state.party != null && this.state.party['office'] !== '') {
       office = <div>
         <h4><b>Office Location:</b></h4>
