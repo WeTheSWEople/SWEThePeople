0371e96c6347bca57e86f408b51d9a12173d9020
Michael Tirtowidjojo <mtirtowidjojo@gmail.com>
2018-04-10 13:57:13 -0500

    Add documentation to backend/util

--- backend/util.py
+++ backend/util.py
@@ -2,24 +2,61 @@ from flask import jsonify
 DATA_NOT_FOUND_CODE = 404
 
 def get_response(data):
-	if data is None:
-		return None
-	else:
-		return data.format()
+    """
+    Gets the formatted response for the given item
+
+    data -- the item
+
+    Returns data.format() or None if data is None
+    """
+
+    if data is None:
+        return None
+    return data.format()
 
 def error(message):
-	resp = jsonify({"Error": message})
-	resp.status_code = DATA_NOT_FOUND_CODE
-	return resp
+    """
+    Builds an error response with the given message
+
+    message -- the message to include
+
+    Returns the error JSON
+    """
+
+    resp = jsonify({"Error": message})
+    resp.status_code = DATA_NOT_FOUND_CODE
+    return resp
 
 def get_all_items(model, orderid, modelname):
-	data = model.query.order_by(orderid).limit(500).all()
-	if not data:
-		return error("Item not found for " + modelname + "model")
-	return jsonify([get_response(rep) for rep in data])
+    """
+    Gets all the items from the given model.
+
+    model       -- the model to get items from
+    orderid     -- how to order the results
+    modelname   -- the name of the model
+
+    Returns the jsonified response from each instance or an error if no model
+    found.
+    """
+
+    data = model.query.order_by(orderid).limit(500).all()
+    if not data:
+        return error("Item not found for " + modelname + "model")
+    return jsonify([get_response(rep) for rep in data])
 
 def get_single_item(model, model_id, parameter):
-	data = model.query.filter(model_id == parameter).first()
-	if not data:
-		return error("Item not found for id " + parameter)
-	return jsonify(get_response(data))
+    """
+    Gets a single item from the given model matching the parameter.
+
+    model       -- the model to get from
+    model_id    -- the column to match against
+    parameter   -- the value to match
+
+    Returns the jsonified response for a single item or an error if no instance
+    found.
+    """
+
+    data = model.query.filter(model_id == parameter).first()
+    if not data:
+        return error("Item not found for id " + parameter)
+    return jsonify(get_response(data))
